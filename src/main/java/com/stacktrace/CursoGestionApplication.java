2f488bf47c78c32fbe170ab1256f60dabc56f619
jeperello <jorge@alasit.com>
2019-03-19 13:43:50 -0300

    First Commit

---
+++ src/main/java/com/stacktrace/CursoGestionApplication.java
@@ -0,0 +1,13 @@
+package com.stacktrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoGestionApplication.class, args);
+	}
+
+}
